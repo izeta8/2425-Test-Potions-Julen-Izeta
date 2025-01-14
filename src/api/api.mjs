4c8cb6e6e5aff66e60bbdaa1812310d068e6d828
izeta8 <julen.izeta@ikasle.aeg.eus>
2025-01-14 09:52:29 +0100

    Create api to fetch diseases, player data and ingredients.

--- src/api/api.mjs
+++ src/api/api.mjs
@@ -11,3 +11,28 @@ export const getIngredients = async () => {
     throw error;
   }
 }
+
+export const getDiseases = async () => {
+  try {
+    let response = await fetch("https://kaotika-server.fly.dev/diseases");
+    let json = await response.json();
+    let ingredients = json.data;
+    return ingredients;
+  } catch (error) {
+    console.error("Failed to fetch diseases:", error);
+    throw error;
+  }
+}
+
+
+export const getPlayerData = async () => {
+  try {
+    let response = await fetch("https://kaotika-server.fly.dev/players/email/julen.izeta@ikasle.aeg.eus");
+    let json = await response.json();
+    let ingredients = json.data;
+    return ingredients;
+  } catch (error) {
+    console.error("Failed to fetch diseases:", error);
+    throw error;
+  }
+}
